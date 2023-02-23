52001afa5a3d54d0130cde08d5f159ad77d5d241
Layla Hammoud <laylahammoud63@gmail.com>
2023-02-23 23:20:26 +0200

    add a for loop to reset the colors of the boundaries background whenever the game start again

--- game.js
+++ game.js
@@ -1,24 +1,29 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     var start = document.querySelector("#start");
     var end = document.querySelector("#end");
-    var boundries = document.querySelectorAll(".boundary");
+    var boundaries = document.querySelectorAll(".boundary");
     var status = document.querySelector("#status");
     var score = 0;
     var play_mode = false;
     start.addEventListener("mouseover", function () {
         play_mode = true;
         status.innerHTML = "The game has started";
+        for (var i = 0; i < boundaries.length; i++) {
+            boundaries[i].className = "boundary";
+        }
     })
-    for (var i = 0; i < boundries.length; i++) {
-        boundries[i].addEventListener("mouseover", function () {
+    for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].addEventListener("mouseover", function () {
             if (play_mode == true) {
                 status.innerHTML = "You lost";
                 score = -10;
-                for (var i = 0; i < boundries.length; i++){
-                  boundries[i].className="boundary youlose" 
+                for (var i = 0; i < boundaries.length; i++) {
+                    boundaries[i].className = "boundary youlose"
                 }
                 mode = false;
             }
         })
     }
+
+
 })
