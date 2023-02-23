2eb5011fa3b8450b40f5d7dce74af5b6c3ed8f06
Layla Hammoud <laylahammoud63@gmail.com>
2023-02-23 22:48:23 +0200

    Select all the needed element

--- game.js
+++ game.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function (event) { 
-
+var start = document.querySelector("#start");
+var end = document.querySelector("#end");
+var boundries = document.querySelectorAll(".boundary");
+var status = document.querySelector("#status");
     
 })
