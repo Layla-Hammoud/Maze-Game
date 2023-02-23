e3a50fa6736a67030da11ea824a8c98e688738e3
Layla Hammoud <laylahammoud63@gmail.com>
2023-02-23 22:51:23 +0200

    declare the score and the play mode variables

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
-document.addEventListener("DOMContentLoaded", function (event) { 
-var start = document.querySelector("#start");
-var end = document.querySelector("#end");
-var boundries = document.querySelectorAll(".boundary");
-var status = document.querySelector("#status");
-    
+document.addEventListener("DOMContentLoaded", function (event) {
+    var start = document.querySelector("#start");
+    var end = document.querySelector("#end");
+    var boundries = document.querySelectorAll(".boundary");
+    var status = document.querySelector("#status");
+    var score = 0;
+    var play_mode = false;
 })
