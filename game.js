a9dcad3e30eccc6db391550e6dfac8a54e6c170d
Layla Hammoud <laylahammoud63@gmail.com>
2023-02-23 23:12:57 +0200

    add the event listener on the boundries to stop whenever the cursor touch a boundry the game and tell the user that he or she lost

--- game.js
+++ game.js
@@ -5,8 +5,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var status = document.querySelector("#status");
     var score = 0;
     var play_mode = false;
-    start.addEventListener("mouseover",function(){
+    start.addEventListener("mouseover", function () {
         play_mode = true;
-        status.innerHTML="The game has started";
+        status.innerHTML = "The game has started";
     })
+    for (var i = 0; i < boundries.length; i++) {
+        boundries[i].addEventListener("mouseover", function () {
+            if (play_mode == true) {
+                status.innerHTML = "You lost";
+                score = -10;
+                for (var i = 0; i < boundries.length; i++){
+                  boundries[i].className="boundary youlose" 
+                }
+                mode = false;
+            }
+        })
+    }
 })
